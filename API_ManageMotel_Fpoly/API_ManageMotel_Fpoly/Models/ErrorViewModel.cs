8da0507dd35a5fa12be250a748d7b72848ad95cc
Khanhvb1904 <khanhcu2k1@gmail.com>
2021-07-16 23:21:43 +0700

    Khung Api

---
+++ API_ManageMotel_Fpoly/API_ManageMotel_Fpoly/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace API_ManageMotel_Fpoly.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

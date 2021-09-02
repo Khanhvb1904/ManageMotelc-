02a9c999e1a24167d5b886fed98df58c66283079
Hung Nguyen Duc <platevn1@gmail.com>
2021-09-03 00:00:13 +0700

    phong_CRUD

---
+++ API_ManageMotel_Fpoly/API_ManageMotel_Fpoly/Controllers/PhongController.cs
@@ -0,0 +1,81 @@
+﻿using API_ManageMotel_Fpoly.EF.Entities;
+using API_ManageMotel_Fpoly.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_ManageMotel_Fpoly.Controllers
+{
+    [Route("[Controller]/")]
+    [ApiController]
+    public class PhongController : ControllerBase
+    {
+        private IPhongService _iPhongService { get; set; }
+        public PhongController(IPhongService phongService)
+        {
+            _iPhongService = phongService;
+        }
+
+        [HttpPost]
+        [Route("TaoPhong")]
+        public async Task<IActionResult> TaoPhong(Phong model)
+        {
+            if (model != null)
+            {
+               await _iPhongService.TaoPhong(model);
+                return Content("Tạo thành công");
+            }
+            return NotFound("Tạo thất bại");
+        }
+
+       
+        [HttpPatch]
+        [Route("SuaPhong/{Id}")]
+        public async Task<IActionResult> SuaPhong(int Id, Phong model)
+        {
+            if (model != null)
+            {
+                await _iPhongService.SuaPhong(Id, model);
+                return Content("Sửa thành công");
+            }
+            return NotFound("Sửa thất bại");
+        }
+
+        
+        [HttpDelete]
+        [Route("XoaPhong/{Id}")]
+        public async Task<IActionResult> XoaPhong(int Id)
+        {
+            if (Id > 0)
+            {
+                await _iPhongService.XoaPhong(Id);
+                return Content("Xóa thành công!");
+            }
+            return NotFound("Không tìm thấy phòng cần xóa!");
+        }
+
+        
+        [HttpGet]
+        [Route("Phong/{Id}")]
+        public async Task<Phong> LayThongTinPhong(int Id)
+        {
+            if (Id > 0)
+            {
+                return await _iPhongService.LayThongTinPhong(Id);
+            }
+            return null;
+
+        }
+
+        
+        [HttpGet]
+        [Route("DanhSachPhong")]
+        public async Task<List<Phong>> DanhSachPhong()
+        {
+            return await _iPhongService.DanhSachPhong();
+        }
+    }
+}
---
+++ API_ManageMotel_Fpoly/API_ManageMotel_Fpoly/Services/PhongService.cs
@@ -0,0 +1,85 @@
+﻿using API_ManageMotel_Fpoly.EF.Entities;
+using API_ManageMotel_Fpoly.EF.ManageMotelDbContext;
+using API_ManageMotel_Fpoly.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_ManageMotel_Fpoly.Services
+{
+    public class PhongService : IPhongService
+    {
+        private readonly ManageMotelDbContext _context;
+        public PhongService(ManageMotelDbContext context)
+        {
+            _context = context;
+        }
+        public Task<Phong> LayThongTinPhong(int Id)
+        {
+            var phong = _context.Phong.Where(c => c.Id == Id).FirstOrDefault();
+            if (phong != null)
+            {
+                return Task.FromResult(phong);
+            }
+            return null;
+        }
+
+        public async Task<int> SuaPhong(int Id, Phong model)
+        {
+            var phong = _context.Phong.Where(c => c.Id == Id).FirstOrDefault();
+            if (phong != null)
+            {
+                phong.maNhaTro = model.maNhaTro;
+                phong.maLoaiPhong = model.maLoaiPhong;
+                phong.soPhong = model.soPhong;
+                phong.giaPhong = model.giaPhong;
+                phong.moTa = model.moTa;
+                phong.sucChua = model.sucChua;
+                phong.dienTich = model.dienTich;
+                phong.ngayPhongSeTrong = model.ngayPhongSeTrong;
+                phong.trangThai = model.trangThai;
+                _context.Phong.Update(phong);
+                await _context.SaveChangesAsync();
+                return 1;
+            }
+            return 0;
+        }
+
+        public async Task<int> TaoPhong(Phong model)
+        {
+            if (model != null)
+            {
+                _context.Phong.Add(model);
+                await _context.SaveChangesAsync();
+                return 1;
+            }
+            return 0;
+        }
+
+        public async Task<int> XoaPhong(int Id)
+        {
+            var phong = _context.Phong.Where(c => c.Id == Id).FirstOrDefault();
+            if (phong!=null){
+                _context.Phong.Remove(phong);
+                await _context.SaveChangesAsync();
+                return 1;
+            }
+            return 0;
+        }
+
+        public async Task<List<Phong>> DanhSachPhong()
+        {
+            var lstPhong = new List<Phong>();
+            var lstPhong_linQ = _context.Phong.ToList();
+            if (lstPhong_linQ.Count > 0)
+            {
+                foreach(var item in lstPhong_linQ)
+                {
+                    lstPhong.Add(item);
+                }
+            }
+            return await Task.FromResult(lstPhong);
+        }
+    }
+}

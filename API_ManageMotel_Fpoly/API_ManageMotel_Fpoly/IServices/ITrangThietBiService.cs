9ab9d6b66eab707e19528d2d588db61fe5184479
Hung Nguyen Duc <platevn1@gmail.com>
2021-09-03 22:46:50 +0700

    dichvu_CRUD, thietbi_CRUD

---
+++ API_ManageMotel_Fpoly/API_ManageMotel_Fpoly/IServices/ITrangThietBiService.cs
@@ -0,0 +1,22 @@
+﻿using API_ManageMotel_Fpoly.EF.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_ManageMotel_Fpoly.IServices
+{
+    public interface ITrangThietBiService
+    {
+        public Task<int> TaoLoaiThietBi(LoaiThietBi model);
+        public Task<int> SuaLoaiThietBi(int Id,LoaiThietBi model);
+        public Task<int> XoaLoaiThietBi(int Id);
+        public Task<int> TaoThietBi(TrangThietBi model);
+        public Task<int> SuaThietBi(int Id,TrangThietBi model);
+        public Task<int> XoaThietBi(int Id);
+        public Task<List<TrangThietBi>> DanhSachThietBi();
+        public Task<TrangThietBi> ThongTinThietBi(int Id);
+        public Task<List<LoaiThietBi>> DanhSachLoaiThietBi();
+        public Task<LoaiThietBi> ThongTinLoaiThietBi(int Id);
+    }
+}
